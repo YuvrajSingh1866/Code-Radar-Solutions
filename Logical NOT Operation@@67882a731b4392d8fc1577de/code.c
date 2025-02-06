1541efcce0be1c5b884a7a6fc925a99fcc0e9bb7
YuvrajSingh1866 <yuvraj1866.becse24@chitkara.edu.in>
2025-02-06 22:13:07 +0530

    Solution to Logical NOT Operation

--- Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -3,8 +3,6 @@
 int main() {
     int num;
     
-    // Input the number
-    printf("Enter a number: ");
     scanf("%d", &num);
 
     // Check if the number is not greater than 0 using the ! operator
