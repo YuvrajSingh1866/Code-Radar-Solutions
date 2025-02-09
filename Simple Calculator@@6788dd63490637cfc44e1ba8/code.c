13fa1291adcbe239b9b04dbff1e7663d4af556f9
YuvrajSingh1866 <yuvraj1866.becse24@chitkara.edu.in>
2025-02-09 16:24:02 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int main() {
-    double num1, num2;
-    char op;
-
-    // Ask user for inpu
-    scanf("%lf", &num1);
-    scanf(" %c", &op);  // Space before %c to capture any leftover newline character
-    scanf("%lf", &num2);
-
-    // Perform calculation based on operator
-    switch (operator) {
-        case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
-            break;
-        case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
-            break;
-        case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
-            break;
-        case
+int main(){
+    int a,b;
+    char ch;
+    scanf("%d %d",&a,&b);
+    scanf("%c",ch);
+    if(ch == '+'){
+        printf("%d", a+b);
     }
-}
+    else if(ch == '-'){
+        printf("%d",a-b);
+    }
+    else if(ch == '*'){
+        printf("%d",a*b);
+    }
+    else{
+        printf("%d",a/b);
+    }
+} 
