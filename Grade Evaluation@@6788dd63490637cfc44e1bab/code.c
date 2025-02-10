d00b9263ab70ea5a42600e7208275ddce2f7d266
YuvrajSingh1866 <yuvraj1866.becse24@chitkara.edu.in>
2025-02-10 18:44:39 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    float marks;
+    scanf("%f", &marks);
+
+    // Check the range and display the grade
+    if (marks >= 90 && marks <= 100) {
+        printf("Grade: A\n");
+    } else if (marks >= 80 && marks < 90) {
+        printf("Grade: B\n");
+    } else if (marks >= 70 && marks < 80) {
+        printf("Grade: C\n");
+    } else if (marks >= 60 && marks < 70) {
+        printf("Grade: D\n");
+    } else if (marks >= 50 && marks < 60) {
+        printf("Grade: E\n");
+    } else if (marks >= 0 && marks < 50) {
+        printf("Grade: F\n");
+    } else {
+        printf("Invalid marks\n");
+    }
+
+    return 0;
+}
