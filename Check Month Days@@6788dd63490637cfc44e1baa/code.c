44a7d02f6c35081c9ce3df78221703859bd5095c
YuvrajSingh1866 <yuvrajsingh58.2012@gmail.com>
2025-02-18 13:36:18 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,62 +1,16 @@
-#include <stdio.h>
-
-#include <stdio.h>
-
-// Function to check if a year is a leap year
-int isLeapYear(int year) {
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        return 1; // It's a leap year
-    }
-    return 0; // It's not a leap year
-}
-
-int main() {
+#include<stdio.h>
+int main(){
     int month;
-
-    scanf("%d", &month);
-
-    // Display the number of days in the month
-    switch (month) {
-        case 1: // January
-            printf("31");
-            break;
-        case 2: // February
-            printf("28\n");
-            break;
-        case 3: // March
-            printf("31\n");
-            break;
-        case 4: // April
-            printf("30\n");
-            break;
-        case 5: // May
-            printf("31\n");
-            break;
-        case 6: // June
-            printf("30\n");
-            break;
-        case 7: // July
-            printf("31\n");
-            break;
-        case 8: // August
-            printf("31\n");
-            break;
-        case 9: // September
-            printf("30\n");
-            break;
-        case 10: // October
-            printf("31\n");
-            break;
-        case 11: // November
-            printf("30\n");
-            break;
-        case 12: // December
-            printf("31\n");
-            break;
-        default:
-            printf("Invalid month\n");
-            break;
+    scanf("%d",&month);
+    switch(month){
+        case 1:
+        printf("31");
+        break;
+        case 2:
+        printf("28");
+        break;
+        case 3:
+        printf("31");
+        break;
     }
-
-    return 0;
 }
