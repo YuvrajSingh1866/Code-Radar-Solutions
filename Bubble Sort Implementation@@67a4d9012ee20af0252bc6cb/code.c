4c0859f8577bbe65fa6c79ecf86a2d5058bfaafa
YuvrajSingh1866 <yuvrajsingh58.2012@gmail.com>
2025-03-19 18:03:39 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,34 +1,42 @@
 #include<stdio.h>
 
+// Function to swap two integers
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-int bubblesort(int arr[],int n){
-    for(int i=1;i<n;i++){
-        for(int j=0;j<n-1;j++){
-            if(arr[i]>arr[j]){
-                swap(arr[i],arr[j]);
+
+// Bubble sort function
+void bubblesort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
             }
         }
     }
 }
-void printArray(int arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d",arr[i]);
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
 }
 
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    bubblesort(arr,n);
-    printArray(arr,n);
+    printf("Original array: \n");
+    printArray(arr, n);
+
+    bubblesort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
     return 0;
 }
