84beba42d4ceb5291fcf818c3a9b6985d2e12926
YuvrajSingh1866 <yuvrajsingh58.2012@gmail.com>
2025-03-19 18:21:06 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -7,33 +7,40 @@ void swap(int *a, int *b) {
     *b = temp;
 }
 
-void result(int arr[],int n){
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                swap(&arr[i],&arr[j]);
+// Function to sort the array in ascending order
+void result(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                swap(&arr[i], &arr[j]);
             }
         }
     }
 }
-void askarray(int arr[],int n){
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+// Function to take array input
+void askarray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-}
-void printarray(int arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d",arr[i]);
-    }
-    printf("\n");
 }
 
-int main(){
+// Function to print the array
+void printarray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);  // Added space for clarity
+    }
+    printf("\n");  // Added newline after printing the array
+}
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    askarray(arr,n);
-    result(arr,n);
-    printarray(arr,n);
-        
+    askarray(arr, n);
+
+    result(arr, n);
+    printarray(arr, n);
+
+    return 0;
 }
